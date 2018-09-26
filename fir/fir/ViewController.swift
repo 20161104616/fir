e12958b290953cb3e1e645cc6c0a101261f7e796
20161104616 <992846873@qq.com>
2018-09-26 12:07:05 +0800

    9.26. 12.06am
    1

---
+++ fir/fir/ViewController.swift
@@ -0,0 +1,59 @@
+//
+//  ViewController.swift
+//  fir
+//
+//  Created by 20161104616 on 2018/9/26.
+//  Copyright © 2018年 20161104616. All rights reserved.
+//
+
+import UIKit
+var base1 = 1;
+var base2 = 1;
+class ViewController: UIViewController {
+
+    @IBOutlet weak var test1: UITextField!
+    
+    @IBOutlet weak var test2: UITextField!
+    
+    @IBAction func zero(_ sender: Any) {
+    }
+    
+    @IBAction func one(_ sender: Any) {
+    }
+    @IBAction func two(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func eight(_ sender: Any) {
+    }
+    @IBAction func nine(_ sender: Any) {
+    }
+    @IBAction func decilmal(_ sender: Any) {   //小数点
+    }
+    @IBAction func add(_ sender: Any) {  // 加
+    }
+    @IBAction func minus(_ sender: Any) {   //减
+    }
+    @IBAction func multiple(_ sender: Any) {  //乘
+    }
+    @IBAction func except(_ sender: Any) { //除
+    }
+    @IBAction func equal(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
