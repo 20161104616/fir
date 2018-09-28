024f1ccb48f96902cd010e3df09e5c74a76d049e
20161104616 <992846873@qq.com>
2018-09-28 17:49:39 +0800

    9.28
    9.28

--- fir/fir/ViewController.swift
+++ fir/fir/ViewController.swift
@@ -11,43 +11,26 @@ var base1 = 1;
 var base2 = 1;
 class ViewController: UIViewController {
 
-    @IBOutlet weak var test1: UITextField!
+    @IBOutlet weak var nu1: UILabel!
     
-    @IBOutlet weak var test2: UITextField!
+    @IBOutlet weak var cs: UITextField!
     
-    @IBAction func zero(_ sender: Any) {
+
+   
+    
+    @IBAction func number(_ sender: AnyObject) {
+        let num = sender.currentTitle
+        nu1.text = nu1.text! + num!!
+        base1 = nu1.text
+        cs.text = base1
     }
     
-    @IBAction func one(_ sender: Any) {
+    @IBAction func calculate(_ sender: AnyObject) {
+        
     }
-    @IBAction func two(_ sender: Any) {
-    }
-    @IBAction func three(_ sender: Any) {
-    }
-    @IBAction func four(_ sender: Any) {
-    }
-    @IBAction func five(_ sender: Any) {
-    }
-    @IBAction func six(_ sender: Any) {
-    }
-    @IBAction func seven(_ sender: Any) {
-    }
-    @IBAction func eight(_ sender: Any) {
-    }
-    @IBAction func nine(_ sender: Any) {
-    }
-    @IBAction func decilmal(_ sender: Any) {   //小数点
-    }
-    @IBAction func add(_ sender: Any) {  // 加
-    }
-    @IBAction func minus(_ sender: Any) {   //减
-    }
-    @IBAction func multiple(_ sender: Any) {  //乘
-    }
-    @IBAction func except(_ sender: Any) { //除
-    }
-    @IBAction func equal(_ sender: Any) {
-    }
+    
+
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
