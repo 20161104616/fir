5ef93803d7f9302835328bf1ac6ce91707558920
20161104616 <992846873@qq.com>
2018-10-17 11:56:35 +0800

    10.17
    10.17

--- fir/fir/ViewController.swift
+++ fir/fir/ViewController.swift
@@ -7,31 +7,65 @@
 //
 
 import UIKit
-var base1 = 1.0;
-var base2 = 1;
+var base1 = "";
+var base2 = "";
+let str1: String = "0"
 class ViewController: UIViewController {
 
     @IBOutlet weak var nu1: UILabel!
     
     @IBOutlet weak var cs: UITextField!
     
-
+    @IBOutlet weak var cs1: UITextField!
+    
    
     
     @IBAction func number(_ sender: AnyObject) {
         let num = sender.currentTitle
         nu1.text = nu1.text! + num!!
+        base1 = nu1.text!;
+        cs.text = base1
 
     }
     
-    @IBAction func calculate(_ sender: AnyObject) {
-        let cal = sender.currentTitle
-        nu1.text = nu1.text! + cal!!
-    //    base1 = Double(nu1.text!)!;
-    }
+//    @IBAction func calculate(_ sender: AnyObject) {
+//        let cal = sender.currentTitle
+//        nu1.text = nu1.text! + cal!!
+//        base1 = nu1.text!;
+//        cs1.text = base1
+//    }
     
-
    
+    @IBAction func equal(_ sender: Any) {
+        base1 = nu1.text!;
+        var i = 0;
+        for i in 0..<base1.count{
+            if(i != 0-9){
+                
+            }
+        }
+            
+        }
+        
+    
+    
+//    button.addTarget(self,action:#selector(tapped),forControlEvents:.TouchUpInside)
+//    func tapped(){
+//        print("tapped")
+//    }
+//
+//
+    
+//    let str = "Hello, my name is Ben. Please visit my ben website at https://www.forta.com/."
+//    label.attributedText = textRegex(pattern: ".a.",str: str, font: 22)
+//
+//    作者：IIronMan
+//    链接：https://www.jianshu.com/p/56da83a4e0ab
+//    來源：简书
+//    简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,3 +75,10 @@ class ViewController: UIViewController {
 
 }
 // a按下运算符时储存前一个数
+
+
+
+let email = "lifusc148387@qq.com"
+let rangeindex = email.range(of: "[0-9]{4}", options: .regularExpression, range: email.startIndex..<email.endIndex, locale:Locale.current)
+//print(email.substring(with: rangeindex!)) //输出；1483
+
