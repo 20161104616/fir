776b75a1d2176fd00ebab88b8c539b9c572495d7
20161104616 <992846873@qq.com>
2018-10-31 11:59:45 +0800

    10.31
    10.31

--- fir/fir/ViewController.swift
+++ fir/fir/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
         nu1.text = nu1.text! + num!!
         base1 = nu1.text!;
         cs.text = base1
-
+        
     }
     
 //    @IBAction func calculate(_ sender: AnyObject) {
@@ -37,55 +37,8 @@ class ViewController: UIViewController {
     
    
     @IBAction func equal(_ sender: Any) {
-        base1 = nu1.text!;
         
-        for ch1 in base1.utf8{
-            if ch1 == "+" {
-                print(
-            }
-            
-          //  print("\(ch1)")
-          //  print("\n")
-        }
-      /*  var i = 0;
-        for i in 0..<base1.count{
-            
-            */
-            /*if(i != 0 && i != 1 && i != 2 && i != 3 && i != 4 && i != 5 && i != 6 && i != 7 && i != 8 && i != 9 ){
-                
-            }*/
-        
-            
-        }
-        
-    
-    
-//    button.addTarget(self,action:#selector(tapped),forControlEvents:.TouchUpInside)
-//    func tapped(){
-//        print("tapped")
-//    }
-//
-//
-    
-//    let str = "Hello, my name is Ben. Please visit my ben website at https://www.forta.com/."
-//    label.attributedText = textRegex(pattern: ".a.",str: str, font: 22)
-//
-//    作者：IIronMan
-//    链接：https://www.jianshu.com/p/56da83a4e0ab
-//    來源：简书
-//    简书著作权归作者所有，任何形式的转载都请联系作者获得授权并注明出处。
-    
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
-}
-// a按下运算符时储存前一个数
 
 
 
