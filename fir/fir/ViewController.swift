334187f14f6526c752b682007218c3a6fead90e9
20161104616 <992846873@qq.com>
2018-11-16 17:42:13 +0800

    11.16
    11.16

--- fir/fir/ViewController.swift
+++ fir/fir/ViewController.swift
@@ -7,27 +7,77 @@
 //
 
 import UIKit
-var base1 = "";
+var base1: Double = 0,base2: Double = 0, base3 = ""
+var counter = 0
+var symboll: String  = ""
 let str1: String = "0"
-var base2 = [Int](repeatElement(0, count: 20))
+//var base2 = [Int](repeatElement(0, count: 20))
 class ViewController: UIViewController {
 
     @IBOutlet weak var nu1: UILabel!
+    @IBOutlet weak var nu2: UILabel!
     
     @IBOutlet weak var cs: UITextField!
     
     @IBOutlet weak var cs1: UITextField!
     
-   
+    
+    
     
     @IBAction func number(_ sender: AnyObject) {
         let num = sender.currentTitle
         nu1.text = nu1.text! + num!!
-        base1 = nu1.text!;
-        cs.text = base1
-        
+        base1 = Double(nu1.text!)!;
+        cs.text = String(base1)
+    }
+      
+    @IBAction func add(_ sender: Any) {
+        //调用等于
+        symboll = "+";
+         counter = 1;
+       // base1 = base1 + base2
+        nu2.text = String(base1)
+        nu1.text = ""
+        base2 = base1
+       
     }
     
+    @IBAction func reduce(_ sender: Any) {
+        symboll = "-";
+        counter = 1;
+       // base1 = base1 - base2
+        nu2.text = String(base1)
+        nu1.text = ""
+        base2 = base1
+        
+    }
+    @IBAction func ride(_ sender: Any) {
+        symboll = "*";
+         counter = 1;
+       
+        nu2.text = String(base1)
+        nu1.text = ""
+        base2 = base1
+       
+    }
+    @IBAction func except(_ sender: Any) {
+        symboll = "/";
+        counter = 1;
+      //  base1 = base1 / 1
+        nu2.text = String(base1)
+        nu1.text = ""
+        base2 = base1
+    }
+    
+    
+    
+    /*       for i in base1 {
+            if i == "+" || i == "-" || i == "*" || i == "/"{
+                print(i)
+            }
+        }
+    }
+    */
 //    @IBAction func calculate(_ sender: AnyObject) {
 //        let cal = sender.currentTitle
 //        nu1.text = nu1.text! + cal!!
@@ -37,12 +87,23 @@ class ViewController: UIViewController {
     
    
     @IBAction func equal(_ sender: Any) {
-        
+        switch symboll {
+        case "+" :
+            base1 = base1 + base2
+            nu1.text = String(base1)
+        case "-" :
+            base1 = base1 - base2
+            nu1.text = String(base1)
+        nu2.text = String(base1)
+        case "*" :
+        base1 = base1 * base2
+            nu1.text = String(base1)
+        case "/" :
+        base1 = base1 / base2
+            nu1.text = String(base1)
+        default:
+            <#code#>
+        }
+
     }
-
-
-
-let email = "lifusc148387@qq.com"
-let rangeindex = email.range(of: "[0-9]{4}", options: .regularExpression, range: email.startIndex..<email.endIndex, locale:Locale.current)
-//print(email.substring(with: rangeindex!)) //输出；1483
-
+}
