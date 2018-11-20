2c00a74a6783b27a0dbfcd55279c286badb924f5
20161104616 <992846873@qq.com>
2018-11-20 17:40:52 +0800

    11.20
    11.20
    可以计算括号内容
    未解决：
    	按下 = 后再按数字不能清空重新计算，而是在数字后面接着输入
    
    bug：
    
    按下  = 后 再按计算符 有bug

--- fir/fir/ViewController.swift
+++ fir/fir/ViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
-var base1: Double = 0,base2: Double = 0, base3 = ""
+var base1: Double = 0,base2: Double = 0, base3: Double = 0
 var counter = 0
 var symboll: String  = ""
+var symboll2: String  = ""
 let str1: String = "0"
 //var base2 = [Int](repeatElement(0, count: 20))
 class ViewController: UIViewController {
@@ -33,16 +34,19 @@ class ViewController: UIViewController {
       
     @IBAction func add(_ sender: Any) {
         //调用等于
+        equal(symboll);
         symboll = "+";
          counter = 1;
        // base1 = base1 + base2
         nu2.text = String(base1)
         nu1.text = ""
         base2 = base1
-       
+        
+    
     }
     
     @IBAction func reduce(_ sender: Any) {
+        equal(symboll);
         symboll = "-";
         counter = 1;
        // base1 = base1 - base2
@@ -50,41 +54,65 @@ class ViewController: UIViewController {
         nu1.text = ""
         base2 = base1
         
+        
+  
+        
     }
     @IBAction func ride(_ sender: Any) {
+        equal(symboll);
         symboll = "*";
          counter = 1;
        
         nu2.text = String(base1)
         nu1.text = ""
         base2 = base1
+        
+        
        
     }
+
     @IBAction func except(_ sender: Any) {
+        equal(symboll);
         symboll = "/";
         counter = 1;
       //  base1 = base1 / 1
         nu2.text = String(base1)
         nu1.text = ""
         base2 = base1
+        
+     
+    }
+    @IBAction func left(_ sender: Any) {
+        base3 = base1;
+        symboll2 = symboll;
+       // clearall(<#Any#>);
+        base1 = 0;
+        base2 = 0;
+        nu1.text = ""
+        nu2.text = ""
+        symboll = ""
+        
+        
+    }
+    @IBAction func right(_ sender: Any) {
+        equal(symboll);
+        symboll = symboll2;
+        base2 = base3;
+        equal(symboll);
+        
     }
     
     
     
-    /*       for i in base1 {
-            if i == "+" || i == "-" || i == "*" || i == "/"{
-                print(i)
-            }
-        }
+    @IBAction func clearall(_ sender: Any) {
+        base1 = 0;
+        base2 = 0;
+        nu1.text = ""
+        nu2.text = ""
+        symboll = ""
     }
-    */
-//    @IBAction func calculate(_ sender: AnyObject) {
-//        let cal = sender.currentTitle
-//        nu1.text = nu1.text! + cal!!
-//        base1 = nu1.text!;
-//        cs1.text = base1
-//    }
     
+
    
     @IBAction func equal(_ sender: Any) {
         switch symboll {
@@ -92,18 +120,18 @@ class ViewController: UIViewController {
             base1 = base1 + base2
             nu1.text = String(base1)
         case "-" :
-            base1 = base1 - base2
+            base1 = base2 - base1
             nu1.text = String(base1)
         nu2.text = String(base1)
         case "*" :
         base1 = base1 * base2
             nu1.text = String(base1)
         case "/" :
-        base1 = base1 / base2
+        base1 = base2 / base1
             nu1.text = String(base1)
         default:
-            <#code#>
+            print("?")
         }
-
+        symboll = ""
     }
 }
